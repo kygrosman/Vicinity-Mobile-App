b9b06fd1a3d8f49fba2f93a764c463d4a25cddf3
kygrosman <grosmankyle@icloud.com>
2022-07-27 14:18:15 -0400

    first push

---
+++ Vicinity/VicinityApp.swift
@@ -0,0 +1,17 @@
+//
+//  VicinityApp.swift
+//  Vicinity
+//
+//  Created by Kyle Grosman on 7/27/22.
+//
+
+import SwiftUI
+
+@main
+struct VicinityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
